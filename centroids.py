0f86ed97ad4ac2fdcf919cc99b7e68a6148ae199
Erick Siavichay <ericksiavichay40@gmail.com>
2023-08-19 20:08:36 -0700

    computed centroids

--- centroids.py
+++ centroids.py
@@ -8,11 +8,12 @@ import constants
 from llama_index.embeddings import OpenAIEmbedding
 import numpy as np
 import openai
+import json
 
 openai.api_key = config.jason_key
 
 
-def compute_query_centroids(queries, path="./query_centroids.npy"):
+def compute_query_centroids(queries, path="./query_centroid.npy"):
     """
     Given a list of queries as strings, returns the mean of the embeddings as a numpy array
     """
@@ -23,5 +24,22 @@ def compute_query_centroids(queries, path="./query_centroids.npy"):
     np.save(path, centroid)
 
 
+def compute_index_centroid(
+    index_path="./indices/arize_1000", path="./arize_1000_centroid.npy"
+):
+    """
+    Given an index, returns the mean of the embeddings as a numpy array
+    """
+    if path is not None:
+        index_name = index_path.split("/")[-1]
+        path = "./" + index_name + "_centroid.npy"
+    vector_store_json = json.load(open(index_path + "/vector_store.json"))
+    document_embeddings = list(vector_store_json["embedding_dict"].values())
+    centroid = np.mean(document_embeddings, axis=0)
+    np.save(path, centroid)
+
+
 if __name__ == "__main__":
+    index_path = "./indices/arize_1000"
     compute_query_centroids(constants.questions)
+    compute_index_centroid()
